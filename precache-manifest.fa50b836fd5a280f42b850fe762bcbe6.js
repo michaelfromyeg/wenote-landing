0f2307ae2641eb6131fb08516db55bafa87cd133
Michael DeMarco <michaelfromyeg@gmail.com>
2020-07-04 16:58:22 -0600

    Updates

---
+++ precache-manifest.fa50b836fd5a280f42b850fe762bcbe6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e6c8175f10e13cada85fafba9fac8d",
+    "url": "/wenote/index.html"
+  },
+  {
+    "revision": "e84aa713a19676de1898",
+    "url": "/wenote/static/css/main.3348d904.chunk.css"
+  },
+  {
+    "revision": "3b810723d6c5bfc18c78",
+    "url": "/wenote/static/js/2.1c442ea4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wenote/static/js/2.1c442ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84aa713a19676de1898",
+    "url": "/wenote/static/js/main.64d3aa15.chunk.js"
+  },
+  {
+    "revision": "774056e1e6f4940355b3",
+    "url": "/wenote/static/js/runtime-main.7a30e9c5.js"
+  },
+  {
+    "revision": "c6a8b35842b48515d7ee6994b38da0db",
+    "url": "/wenote/static/media/dots.c6a8b358.png"
+  },
+  {
+    "revision": "305707c3ff021e874a07fdd851bcea02",
+    "url": "/wenote/static/media/header.305707c3.png"
+  },
+  {
+    "revision": "f74729363fb4c18418fa14391db92f25",
+    "url": "/wenote/static/media/mac.f7472936.png"
+  }
+]);
